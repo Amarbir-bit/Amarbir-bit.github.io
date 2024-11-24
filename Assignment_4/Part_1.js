6666e2eaf9571df6e224232b7133c4f68f1a8ab9
Amarbir-bit <amarbirsingh3@dcmail.ca>
2024-11-24 14:42:55 -0500

    Bug fixed

--- Assignment_4/Part_1.js
+++ Assignment_4/Part_1.js
@@ -11,7 +11,7 @@ function randomValueFromArray(array){
 
 //2. RAW TEXT STRINGS
 
-let storyTexT =  "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
+let storyText =  "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
 
 const insertX = ['Willy the Goblin', 
                 'Big Daddy', 
@@ -58,5 +58,5 @@ function result() {
 
   story.textContent = newStory;
   story.style.visibility = 'visible';
-  
+
 }
