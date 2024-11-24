1ca8811e2d3fe610cc1b00b157611c718023f152
Amarbir-bit <amarbirsingh3@dcmail.ca>
2024-11-24 16:11:06 -0500

    Declaring const array and object

--- Assignment_4/Part_2.js
+++ Assignment_4/Part_2.js
@@ -5,8 +5,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const pictures = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
 
 /* Declaring the alternative text for each image file */
+const alternatives = {
+    'pic1.jpg' : 'Human eye',
+    'pic2.jpg' : 'Waves',
+    'pic3.jpg' : 'Flowers',
+    'pic4.jpg' : 'Egyptian Pharoah picture',
+    'pic5.jpg' : 'Butterfly on leaf'
+  }
 
 /* Looping through images */
 
